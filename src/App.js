aa018414ec1509d7e7c3e633e94d532a0141fd87
SwapnilKasliwal <swapnilkasliwal2000@gmail.com>
2023-08-02 16:28:27 +0530

    adding general files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from './Components/pages/HomePage/Homepage';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer'
+import GranitePage from './Components/pages/GranitePage/GranitePage';
+import GalleryPage from './Components/pages/GalleryPage/GalleryPage'
+import ContactPage from './Components/pages/ContactPage/ContactPage';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element={<><Navbar/><Homepage/><Footer/></>}/>
+        <Route path='/granite' element={<><Navbar/><GranitePage/><Footer/></>}/>
+        <Route path='/gallery' element={<><Navbar/><GalleryPage/><Footer/></>}/>
+        <Route path='/granite/product/:id' element={<><Navbar/><GranitePage/><Footer/></>}/>
+        <Route path='/contact' element={<><Navbar/><ContactPage/><Footer/></>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
